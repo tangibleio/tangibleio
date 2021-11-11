b46ab693a3646b4b854072ac1470e29097200125
relno <arielnoyman@gmail.com>
2021-11-11 15:44:29 -0500

    added link to app

--- src/layouts/FooterNav.js
+++ src/layouts/FooterNav.js
@@ -3,24 +3,34 @@ import classNames from "classnames";
 import { Link } from "react-router-dom";
 
 const FooterNav = ({ className, ...props }) => {
-    const classes = classNames("footer-nav", className);
+  const classes = classNames("footer-nav", className);
 
-    return (
-        <nav {...props} className={classes}>
-            <ul className="list-reset">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/#features">Features</Link>
-                </li>
+  return (
+    <nav {...props} className={classes}>
+      <ul className="list-reset">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/#features">Features</Link>
+        </li>
 
-                <li>
-                    <Link to="/#team">Team</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+        <li>
+          <Link to="/#team">Team</Link>
+        </li>
+
+        <li>
+          <a
+            rel="noopener noreferrer"
+            href="https://master.d21x7jmmbknvuw.amplifyapp.com/"
+            target="_blank"
+          >
+            Login
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default FooterNav;
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,151 +4,152 @@ import classNames from "classnames";
 import { Link } from "react-router-dom";
 
 const propTypes = {
-    navPosition: PropTypes.string,
-    hideNav: PropTypes.bool,
-    hideSignin: PropTypes.bool,
-    bottomOuterDivider: PropTypes.bool,
-    bottomDivider: PropTypes.bool,
+  navPosition: PropTypes.string,
+  hideNav: PropTypes.bool,
+  hideSignin: PropTypes.bool,
+  bottomOuterDivider: PropTypes.bool,
+  bottomDivider: PropTypes.bool,
 };
 
 const defaultProps = {
-    navPosition: "",
-    hideNav: false,
-    hideSignin: false,
-    bottomOuterDivider: false,
-    bottomDivider: false,
+  navPosition: "",
+  hideNav: false,
+  hideSignin: false,
+  bottomOuterDivider: false,
+  bottomDivider: false,
 };
 
 const Header = ({
-    className,
-    navPosition,
-    hideNav,
-    hideSignin,
-    bottomOuterDivider,
-    bottomDivider,
-    ...props
+  className,
+  navPosition,
+  hideNav,
+  hideSignin,
+  bottomOuterDivider,
+  bottomDivider,
+  ...props
 }) => {
-    const [isActive, setIsactive] = useState(false);
+  const [isActive, setIsactive] = useState(false);
 
-    const nav = useRef(null);
-    const hamburger = useRef(null);
+  const nav = useRef(null);
+  const hamburger = useRef(null);
 
-    useEffect(() => {
-        isActive && openMenu();
-        document.addEventListener("keydown", keyPress);
-        document.addEventListener("click", clickOutside);
-        return () => {
-            document.removeEventListener("keydown", keyPress);
-            document.addEventListener("click", clickOutside);
-            closeMenu();
-        };
-    });
-
-    const openMenu = () => {
-        document.body.classList.add("off-nav-is-active");
-        nav.current.style.maxHeight = nav.current.scrollHeight + "px";
-        setIsactive(true);
+  useEffect(() => {
+    isActive && openMenu();
+    document.addEventListener("keydown", keyPress);
+    document.addEventListener("click", clickOutside);
+    return () => {
+      document.removeEventListener("keydown", keyPress);
+      document.addEventListener("click", clickOutside);
+      closeMenu();
     };
+  });
 
-    const closeMenu = () => {
-        document.body.classList.remove("off-nav-is-active");
-        nav.current && (nav.current.style.maxHeight = null);
-        setIsactive(false);
+  const openMenu = () => {
+    document.body.classList.add("off-nav-is-active");
+    nav.current.style.maxHeight = nav.current.scrollHeight + "px";
+    setIsactive(true);
+  };
+
+  const closeMenu = () => {
+    document.body.classList.remove("off-nav-is-active");
+    nav.current && (nav.current.style.maxHeight = null);
+    setIsactive(false);
+  };
+
+  const keyPress = (e) => {
+    isActive && e.keyCode === 27 && closeMenu();
+  };
+
+  const clickOutside = (e) => {
+    if (!nav.current) return;
+    if (
+      !isActive ||
+      nav.current.contains(e.target) ||
+      e.target === hamburger.current
+    )
+      return;
+    closeMenu();
+  };
+
+  const [top, setTop] = useState(true);
+
+  // detect whether user has scrolled the page down by 10px
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > "500" ? setTop(false) : setTop(true);
     };
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top]);
 
-    const keyPress = (e) => {
-        isActive && e.keyCode === 27 && closeMenu();
-    };
+  const classes = classNames(
+    !top && "nav-bar-bg-color",
+    top && "invert-color",
+    "site-header",
+    bottomOuterDivider && "has-bottom-divider"
+  );
 
-    const clickOutside = (e) => {
-        if (!nav.current) return;
-        if (
-            !isActive ||
-            nav.current.contains(e.target) ||
-            e.target === hamburger.current
-        )
-            return;
-        closeMenu();
-    };
+  return (
+    <header {...props} className={classes}>
+      <div className="container">
+        <div className={classNames("site-header-inner")}>
+          {!hideNav && (
+            <>
+              <button
+                ref={hamburger}
+                className="header-nav-toggle"
+                onClick={isActive ? closeMenu : openMenu}
+              >
+                <span className="screen-reader">Menu</span>
+                <span className="hamburger">
+                  <span className="hamburger-inner"></span>
+                </span>
+              </button>
 
-    const [top, setTop] = useState(true);
-
-    // detect whether user has scrolled the page down by 10px
-    useEffect(() => {
-        const scrollHandler = () => {
-            window.pageYOffset > "500" ? setTop(false) : setTop(true);
-        };
-        window.addEventListener("scroll", scrollHandler);
-        return () => window.removeEventListener("scroll", scrollHandler);
-    }, [top]);
-
-    const classes = classNames(
-        !top && "nav-bar-bg-color",
-        top && "invert-color",
-        "site-header",
-        bottomOuterDivider && "has-bottom-divider"
-    );
-
-    return (
-        <header {...props} className={classes}>
-            <div className="container">
-                <div className={classNames("site-header-inner")}>
-                    {!hideNav && (
-                        <>
-                            <button
-                                ref={hamburger}
-                                className="header-nav-toggle"
-                                onClick={isActive ? closeMenu : openMenu}
-                            >
-                                <span className="screen-reader">Menu</span>
-                                <span className="hamburger">
-                                    <span className="hamburger-inner"></span>
-                                </span>
-                            </button>
-
-                            <nav
-                                ref={nav}
-                                className={classNames(
-                                    "header-nav",
-                                    isActive && "is-active"
-                                )}
-                            >
-                                <div className="header-nav-inner">
-                                    <ul
-                                        className={classNames(
-                                            "list-reset text-xs",
-                                            navPosition &&
-                                                `header-nav-${navPosition}`
-                                        )}
-                                    >
-                                        <li>
-                                            <Link to="/" onClick={closeMenu}>
-                                                Home
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                to="/#features"
-                                                onClick={closeMenu}
-                                            >
-                                                Features
-                                            </Link>
-                                        </li>
-
-                                        <li>
-                                            <Link to="/#team" onClick={closeMenu}>
-                                                Team
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </nav>
-                        </>
+              <nav
+                ref={nav}
+                className={classNames("header-nav", isActive && "is-active")}
+              >
+                <div className="header-nav-inner">
+                  <ul
+                    className={classNames(
+                      "list-reset text-xs",
+                      navPosition && `header-nav-${navPosition}`
                     )}
+                  >
+                    <li>
+                      <Link to="/" onClick={closeMenu}>
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/#features" onClick={closeMenu}>
+                        Features
+                      </Link>
+                    </li>
+
+                    <li>
+                      <Link to="/#team" onClick={closeMenu}>
+                        Team
+                      </Link>
+                    </li>
+
+                    <a
+                      rel="noopener noreferrer"
+                      href="https://master.d21x7jmmbknvuw.amplifyapp.com/"
+                      target="_blank"
+                    >
+                      Login
+                    </a>
+                  </ul>
                 </div>
-            </div>
-        </header>
-    );
+              </nav>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 };
 
 Header.propTypes = propTypes;
